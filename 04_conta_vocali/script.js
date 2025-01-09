60dc67974034589d68b35dd5890f70be39068950
Giovanni <dibellogio@gmail.com>
2025-01-09 15:38:31 +0100

    add solution es 04 conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,29 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function contaVocali (stringa) {
 
+    // inizializzo la variabile vocali
+    let vocali = 0;
+    
+    // ciclo sulla stringa
+    for (let i = 0; i < stringa.length; i++) {
+
+        // prendo il carattere iesimo
+        let carattere = stringa.charAt(i);   
+        
+        // se il carattere è una vocale
+        if (["a", "e", "i", "o", "u"].includes(carattere)) {
+            // conto la vocale
+            vocali += 1;
+        }
+    }
+
+    return vocali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log("Le vocali sono: ",contaVocali(word));
+ 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
