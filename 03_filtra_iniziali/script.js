f00d041d4b7b4c120e0b3b6700074e5c3e15b0a2
Giovanni <dibellogio@gmail.com>
2025-01-09 15:20:26 +0100

    add solution es 03 filtra iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,32 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+// imposto la lettera in una variabile
+let lettera = "A";
 
 
 // Dichiara la funzione qui.
+function filtraStringhe (nomiArray, letteraDecisa) {
+    // creo un nuovo array
+    const nomiFiltrati = [];
 
+    // ciclo sull'array dei nomi
+    for (let i = 0; i < nomiArray.length; i++) {
+
+        // prendo il nome iesimo
+        let nome = nomiArray[i];
+
+        if (nome.charAt(0) === letteraDecisa) {
+            // metto il nome nel nuovo array
+            nomiFiltrati.push(nome);
+        }
+    }
+
+    return nomiFiltrati;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filtraStringhe(names,lettera));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
